1567eb3c0a866ae61bc843856db8cdd0ed70994e
redsylx <fakhrialbari.red@gmail.com>
2024-08-09 23:32:11 +0700

    add uploadController

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,22 @@
+using Azure.Storage.Blobs;
+using Main.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Main.Controllers;
+
+public class UploadController : ControllerCore {
+    private readonly BlobServiceClient _blobServiceClient;
+    public UploadController(BlobServiceClient blobServiceClient) : base()
+    {
+        _blobServiceClient = blobServiceClient;
+    }
+
+    [HttpGet("image")]
+    [AllowAnonymous]
+    public IActionResult UploadUrl([FromQuery] string fileName)
+    {
+        var uploadService = new FileService(_blobServiceClient);
+        return new OkObjectResult(uploadService.UploadImage(fileName));
+    }
+}
