a57eee94ee0694dc626917a8cd22992ab5c9b60a
redsylx <fakhrialbari.red@gmail.com>
2024-09-08 23:30:06 +0700

    feat: add get lis sellcardcategory api

--- Controllers/SellCardCategoryController.cs
+++ Controllers/SellCardCategoryController.cs
@@ -6,6 +6,9 @@ using AutoMapper;
 using Azure.Storage.Blobs;
 using System;
 using Microsoft.AspNetCore.Authorization;
+using Main.Utils;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Main.Controllers;
 
@@ -19,6 +22,7 @@ public class SellCardCategoryController : ControllerBase<SellCardCategoryControl
         _containerImageName = Environment.GetEnvironmentVariable(EnvironmentVariables.CONTAINER_NAME_IMAGE) ?? "";
         var config = new MapperConfiguration(cfg => 
         {
+            cfg.CreateMap<SellCardCategory, SellCardCategoryDto>();
         });
         _mapper = config.CreateMapper();
     }
@@ -34,4 +38,35 @@ public class SellCardCategoryController : ControllerBase<SellCardCategoryControl
 
         return new OkObjectResult(sellCardCategory);
     }
+
+    [HttpGet]
+    [Route("list/account")]
+    public IActionResult ListByAccount([FromQuery] PaginationRequest paginationRequest, [FromQuery] string accountId) {
+        var sellCardCategoryService = new SellCardCategoryService(_context);
+        var result = sellCardCategoryService.List(paginationRequest, accountId);
+        var listDto = _mapper.Map<List<SellCardCategoryDto>>(result.Items).Select(p => { p.Account = null; return p; }).ToList();
+        var resultDto = new PaginationResult<SellCardCategoryDto>(listDto, result.TotalCount, result.PageNumber, result.MaxPageNumber, result.PageSize);
+        AddSasToken(resultDto.Items);
+        return new OkObjectResult(resultDto);
+    }
+
+    private void AddSasToken(SellCardCategoryDto item) {
+        var fileService = new FileService(_blobServiceClient);
+        if(!string.IsNullOrEmpty(item.Img)) AddSasToken(fileService, item);
+    }
+
+    private void AddSasToken(List<SellCardCategoryDto> items) {
+        var fileService = new FileService(_blobServiceClient);
+        foreach(var item in items) {
+            if(!string.IsNullOrEmpty(item.Img)) AddSasToken(fileService, item);
+        }
+    }
+
+    private void AddSasToken(FileService fileService, SellCardCategoryDto item) {
+        item.ImgUrl = fileService.GetSasToken(_containerImageName, item.Img ?? "");
+    }
+}
+
+public class SellCardCategoryDto : SellCardCategory {
+    public string ImgUrl { get; set; } = "";
 }
